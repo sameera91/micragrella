865c5e12e451210c790ab85e10f4ec8bddf88072
Katherine McClintic <kdmcclin@gmail.com>
2016-06-15 19:18:09 -0400

    Add controller skeletons

---
+++ app/controllers/moths_controller.rb
@@ -0,0 +1,2 @@
+class MothsController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615231118) do
+ActiveRecord::Schema.define(version: 20160615231334) do
+
+  create_table "food_types", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "cost"
+    t.integer  "weight_increase"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "moths", force: :cascade do |t|
     t.string   "name"
